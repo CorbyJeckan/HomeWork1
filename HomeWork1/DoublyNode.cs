713bd4f61e5b24f41b7a3ff494eaa438d70c1ce5
Евгений Савельев <curtisjeckan@mail.ru>
2022-05-30 21:38:53 +0500

    двусвязный список

---
+++ HomeWork1/DoublyNode.cs
@@ -0,0 +1,16 @@
+﻿namespace Thread2447455
+{
+    public class DoublyNode<T>
+    {
+#pragma warning disable CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
+        public DoublyNode(T data)
+#pragma warning restore CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
+        {
+            Data = data;
+        }
+
+        public T Data { get; set; }
+        public DoublyNode<T> Previous { get; set; }
+        public DoublyNode<T> Next { get; set; }
+    }
+}
